1636027813ae5e4d69607889030bef2f34e81c17
John DeWitt <john_dewitt@hotmail.com>
2022-05-24 13:47:42 -0500

    Updated read and assignment of Rock vars

--- processDraftMailMerge.R
+++ processDraftMailMerge.R
@@ -10,6 +10,74 @@ fn <- "2022_Rockets_Draft_Data_Sheet .xlsx"
 df_anthro <- read.xlsx(file.path(inP, fn), detectDates = T) %>%
   janitor::clean_names()
 
+## create output anthro dataframe with new variable names that match mail merge
+df_anthro %>%
+  mutate(Fullname = paste(first_name, last_name)) %>%
+  rename(r_val_Ht_wo_shoes = height_no_shoes, 
+         r_val_StandReach = standing_reach,
+         r_val_Wing = wingspan,
+         r_val_maxVert = max_vertical,
+         r_val_BW_lbs = weight,
+         r_val_standMaxVert = vertical_no_step,
+         r_val_Bfat_pct = body_fat_pct,
+         r_val_HandLen = hand_length,
+         r_val_HandWid = hand_width,
+         r_val_LaneShut_L = pro_lane_agility_l,
+         r_val_LaneShut_R = pro_lane_agility_r,
+         r_val_LaneAgility = lane_agility,
+         r_val_3qrtRock = x3_4_sprint,
+         r_val_10m = x10_m,
+         r_val_QB = quickboard_total,
+         r_val_QB_L = quickboard_l,
+         r_val_QB_R = quickboard_r,
+         r_val_LMM = lean_muscle_mass,
+         val_ProLaneAgility_R = pro_lane_agility_r,
+         val_ProLaneAgility__L = pro_lane_agility_l) %>%
+  mutate(r_val_qb_imbalance = ((r_val_QB_R / r_val_QB_L) - 1) *100)
+
+
+
+# 
+# r_pct_Ht_wo_shoes
+# 
+# r_pct_StandReach
+# 
+# r_pct_Wing
+# r_val_WingHt
+# r_pct_Wing_Ht
+# r_val_JumpHt
+# r_pct_jumpHt
+# 
+# r_pct_stantMaxVert
+# 
+# r_pct_MaxVert
+# 
+# r_pct_BW_lbs
+# 
+# r_pct_Bfat_pct
+# 
+# r_pct_HandLen
+# 
+# r_pct_HandWid
+# 
+# r_pct_LaneShut_R
+# 
+# r_pct_LaneShut_L
+# 
+# r_pct_LaneAgility
+# r_val_3qrtSpeed
+# r_pct_3qrtSpeed
+# 
+# r_pct_3qrtRock
+# 
+# r_pct_QB
+# 
+# r_pct_10m
+# 
+# r_pct_shutLRock
+# 
+# r_pct_shutRRock
+
 ## 2) Get historical combine data
 fn <- "nba-combine-historical-through-2021-results.csv"
 df_combine <- read.csv(file.path(inP, fn), stringsAsFactors = F)
