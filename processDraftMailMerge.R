c43f2d17a981f5dfa9a3b18ceea46d31ed619f3f
John DeWitt <john_dewitt@hotmail.com>
2022-06-01 11:09:58 +0200

    Added summary jump data to output dataframe

--- processDraftMailMerge.R
+++ processDraftMailMerge.R
@@ -169,8 +169,11 @@ allOut <- NULL
 for (r in seq(1, nrow(df_anthro))) {
   fName = df_anthro$first_name[r]
   lName = df_anthro$last_name[r]
+  fullName = df_anthro$fullname[r]
   this_ath <- df_anthro %>% filter(first_name == fName &
                                      last_name == lName)
+  
+  ## anthro
   
   inD = df_combine[df_combine$varName == 'c_val_Ht_wo_shoes', 'value']
   r_pct_Ht_wo_shoes = computePctTile(inD, this_ath$r_val_Ht_wo_shoes)
@@ -214,6 +217,80 @@ for (r in seq(1, nrow(df_anthro))) {
   inD = df_combine[df_combine$varName == 'c_val_3qrtSpeed', 'value']
   r_pct_3qrtRock = computePctTile(inD, this_ath$r_val_3qrtRock, higher_better = F)
   
+  ## force
+  
+  thisath_CMJ <- d_cmj_rocket %>% filter(fullname == fullName)
+  # val_CM_conImp
+  r_val_CM_conImp = thisath_CMJ$concentric_impulse_ns_kg
+  inD <- df_force_hist %>% filter(type == 'Countermovement Jump',
+                           varName == 'concentric_impulse_ns_kg') %>%
+    select(value) %>% pull()
+  r_pct_CM_conImp = computePctTile(inD, r_val_CM_conImp)
+  
+  # val_CM_fzero
+  r_val_CM_fzero = thisath_CMJ$force_at_zero_velocity_n_kg
+  inD <- df_force_hist %>% filter(type == 'Countermovement Jump',
+                                  varName == 'force_at_zero_velocity_n_kg') %>%
+    select(value) %>% pull()
+  r_pct_CM_fzero = computePctTile(inD, r_val_CM_fzero)
+  
+  # val_CM_relPP
+  r_val_CM_relPP = thisath_CMJ$peak_power_bm_w_kg
+  inD <- df_force_hist %>% filter(type == 'Countermovement Jump',
+                                  varName == 'peak_power_bm_w_kg') %>%
+    select(value) %>% pull()
+  r_pct_CM_relPP = computePctTile(inD, r_val_CM_relPP)
+  
+  # val_CM_PP
+  r_val_CM_PP = thisath_CMJ$peak_power_w
+  inD <- df_force_hist %>% filter(type == 'Countermovement Jump',
+                                  varName == 'peak_power_w') %>%
+    select(value) %>% pull()
+  r_pct_CM_PP = computePctTile(inD, r_val_CM_PP)
+  
+  # val_CM_RSImod
+  r_val_CM_RSImod = thisath_CMJ$rsi_modified_m_s
+  inD <- df_force_hist %>% filter(type == 'Countermovement Jump',
+                                  varName == 'rsi_modified_m_s') %>%
+    select(value) %>% pull()
+  r_pct_CM_RSImod = computePctTile(inD, r_val_CM_RSImod)
+  
+  ## drop jump
+  thisath_DJ <- d_dj_rocket %>% filter(fullname == fullName)
+  # val_DJ_CT
+  r_val_DJ_CT = thisath_DJ$contact_time_s
+  inD <- df_force_hist %>% filter(type == 'Drop Jump',
+                                  varName == 'contact_time_s') %>%
+    select(value) %>% pull()
+  r_pct_DJ_CT = computePctTile(inD, r_val_DJ_CT, higher_better = F)
+  
+  # val_DJ_RSI
+  r_val_DJ_RSI = thisath_DJ$rsi_flight_time_contact_time
+  inD <- df_force_hist %>% filter(type == 'Drop Jump',
+                                  varName == 'rsi_flight_time_contact_time') %>%
+    select(value) %>% pull()
+  r_pct_DJ_RSI = computePctTile(inD, r_val_DJ_RSI)
+  
+  ## SLJ jump
+  thisath_SLJ <- d_slj_rocket %>% filter(fullname == fullName)
+  
+  ## val_SL_EccMean_L
+  r_val_SL_EccMean_L = thisath_SLJ$Left_eccentric_mean_force_n_kg
+  inD <- df_force_hist %>% filter(type == 'Single Leg Jump',
+                                  varName == 'Left_eccentric_mean_force_n_kg') %>%
+    select(value) %>% pull()
+  r_pct_SL_EccMean_L = computePctTile(inD, r_val_SL_EccMean_L)
+  
+  ## val_SL_EccMean_R
+  r_val_SL_EccMean_R = thisath_SLJ$Right_eccentric_mean_force_n_kg
+  inD <- df_force_hist %>% filter(type == 'Single Leg Jump',
+                                  varName == 'Right_eccentric_mean_force_n_kg') %>%
+    select(value) %>% pull()
+  r_pct_SL_EccMean_R = computePctTile(inD, r_val_SL_EccMean_R)
+  
+  r_SLJ_Sym = round(((r_val_SL_EccMean_R / r_val_SL_EccMean_L) - 1) * 100,2)
+  
+  ## output dataframe
   this_ath <- cbind(
     this_ath,
     data.frame(
@@ -231,97 +308,33 @@ for (r in seq(1, nrow(df_anthro))) {
       r_pct_HandWid,
       r_pct_LaneShut_R,
       r_pct_LaneShut_L,
-      r_pct_3qrtRock
-    )
+      r_pct_3qrtRock,
+      r_val_CM_conImp,
+      r_pct_CM_conImp,
+      r_val_CM_fzero,
+      r_pct_CM_fzero,
+      r_val_CM_relPP,
+      r_pct_CM_relPP,
+      r_val_CM_PP,
+      r_pct_CM_PP ,
+      r_val_CM_RSImod,
+      r_pct_CM_RSImod ,
+      r_val_DJ_CT,
+      r_pct_DJ_CT,
+      r_val_DJ_RSI,
+      r_pct_DJ_RSI,
+      r_val_SL_EccMean_L,
+      r_pct_SL_EccMean_L,
+      r_val_SL_EccMean_R,
+      r_pct_SL_EccMean_R,
+      r_SLJ_Sym 
+    ) %>% mutate_if(is.numeric, round, 2)
   )
-  allOut <- bind_rows(allOut, this_ath)
+  
+  ## add current athlete data to allOut data frame
+  allOut <- bind_rows(allOut, this_ath) 
 }
 
 ## 5) write to excel file
 write.xlsx(allOut, file = 'docs/draft2022CompiledData.xlsx')
 
-# ## 2) Get Rocket Jump data -----
-# df_force_hist <- read.csv(file = 'allHistoricalJumpData.csv', stringsAsFactors = F)
-# 
-# forceFolder = 'ForceDecks'
-# 
-# ## 2a) CMJ data
-# ## CMJ variables:
-# # val_CM_conImp
-# # pct_CM_comImp
-# # val_CM_fzero
-# # pct_CM_fzero
-# # val_CM_relPP
-# # pct_CM_relPP
-# # val_CM_PP
-# # pct_CM_PP
-# # val_CM_RSImod
-# # pct_CM_RSImod
-# cmjFile <- dir(paste(inP, forceFolder, sep ='/'), pattern = 'CMJ')
-# 
-# d_cmj_rocket <- 
-# read.xlsx(file.path(inP, forceFolder, cmjFile), detectDates = T,
-#           startRow = 8) %>%
-#   janitor::clean_names() %>%
-#   mutate(test_date = janitor::excel_numeric_to_date(test_date)) %>%
-#   select(athlete, test_type, test_date, body_weight_kg, trial,
-#          concentric_impulse_ns, force_at_zero_velocity_n, peak_power_bm_w_kg,
-#          peak_power_w,rsi_modified_m_s) %>%
-#   filter(rsi_modified_m_s < 2) %>%
-#   mutate(concentric_impulse_ns_kg = concentric_impulse_ns / body_weight_kg,
-#          force_at_zero_velocity_n_kg = force_at_zero_velocity_n / body_weight_kg) %>%
-#   gather(varName, value, -athlete, -test_type, -test_date) %>%
-#   filter(varName != 'trial') %>%
-#   mutate(value = as.numeric(value)) %>%
-#   group_by(athlete, test_type, test_date, varName) %>%
-#   summarise(meanVal = mean(value)) %>%
-#   ungroup() %>% spread(varName, meanVal) %>%
-#   rename(fullname = athlete,
-#          type = test_type, date = test_date)
-#   
-# ## 2b) DJ data
-# ## Drop Jump variables
-# ## val_DJ_CT
-# ## pct_DJ_CT
-# ## val_DJ_RSI
-# ## pct_DJ_RSI
-# djFile <- dir(paste(inP, forceFolder, sep ='/'), pattern = 'DJ')
-# d_dj_rocket <- 
-#   read.xlsx(file.path(inP, forceFolder, djFile), detectDates = T,
-#           startRow = 8) %>%
-#   janitor::clean_names() %>%
-#   mutate(test_date = janitor::excel_numeric_to_date(test_date)) %>%
-#   select(athlete, test_type, test_date, body_weight_kg, trial,
-#          contact_time_s, rsi_flight_time_contact_time) %>%
-#   gather(varName, value, -athlete, -test_type, -test_date) %>%
-#   filter(varName != 'trial') %>%
-#   mutate(value = as.numeric(value)) %>%
-#   group_by(athlete, test_type, test_date, varName) %>%
-#   summarise(meanVal = mean(value)) %>%
-#   ungroup() %>% spread(varName, meanVal) %>%
-#   rename(fullname = athlete, type = test_type, date = test_date)
-# 
-# ## 2c) SLJ data
-# ## SLJ variables
-# ## val_SL_EccMean
-# ## pct_SL_EccMean
-# sljFile <- dir(paste(inP, forceFolder, sep ='/'), pattern = 'SLJ')
-# d_slj_rocket <- 
-#   read.xlsx(file.path(inP, forceFolder, sljFile), detectDates = T,
-#             startRow = 8) %>%
-#   janitor::clean_names() %>%
-#   mutate(test_date = janitor::excel_numeric_to_date(test_date)) %>%
-#     select(athlete, test_type, test_date, body_weight_kg, trial,
-#            eccentric_mean_force_n) %>%
-#       mutate(eccentric_mean_force_n_kg = eccentric_mean_force_n / body_weight_kg) %>%
-#     separate(trial, c('leg', 'trial'))  %>%
-#   gather(varName, value, -athlete, -test_type, -test_date, -leg) %>%
-#   filter(varName != 'trial') %>%
-#   mutate(value = as.numeric(value)) %>%
-#   group_by(athlete, test_type, test_date, leg, varName) %>%
-#   summarise(meanVal = mean(value, na.rm = T)) %>%
-#   ungroup() %>% unite('varName', c(leg,varName)) %>% 
-#   spread(varName, meanVal) %>%
-#   rename(fullname = athlete,
-#          type = test_type,
-#          date = test_date)
